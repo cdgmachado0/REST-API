87d541e2a766b22f445b0c49a6a6879afff57f6a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-03-09 22:05:22 +0100

    starting to work on VIRTUAL

--- models/user.js
+++ models/user.js
@@ -5,11 +5,6 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {};
   User.init({
-    // id: {
-    //   type: DataTypes.INTEGER,
-    //   primaryKey: true,
-    //   autoIncrement: true
-    // },
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
